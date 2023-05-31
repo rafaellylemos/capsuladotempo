377dee5c70a5edd74cf881c5d825f5fda5308e12
rafaellylemos <rafaellylemos@hotmail.com>
2023-05-31 17:17:09 -0300

    first commit

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        href="https://www.linkedin.com/in/rafaelly-lemos/"
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+      >
+        Rafaelly
+      </a>{' '}
+      no NLW da{' '}
+      <a
+        href="https://www.rocketseat.com.br"
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+      >
+        Rockeseat
+      </a>
+    </div>
+  )
+}
